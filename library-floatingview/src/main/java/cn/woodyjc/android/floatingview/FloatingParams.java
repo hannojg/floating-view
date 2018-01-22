f8d302239e920f90e515dd4188efa8bf9d5f1615
june545 <545329942@qq.com>
2018-01-22 15:31:38 +0800

    add comment

--- library-floatingview/src/main/java/cn/woodyjc/android/floatingview/FloatingParams.java
+++ library-floatingview/src/main/java/cn/woodyjc/android/floatingview/FloatingParams.java
@@ -4,20 +4,33 @@ import android.util.DisplayMetrics;
 
 /**
  * 悬浮窗参数
- *
+ * <p>
  * Created by June on 2018/1/19.
  */
 
 public class FloatingParams {
-
+	/**
+	 * 宽
+	 */
 	public int width;
+	/**
+	 * 高
+	 */
 	public int height;
+	/**
+	 * 左上角顶点x坐标
+	 */
 	public int x;
+	/**
+	 * 左上角顶点y坐标
+	 */
 	public int y;
+	/**
+	 * 悬浮窗类型 {@link FloatingType}
+	 */
+	public int type;
 
-	public int type; // 显示级别
-
-	public FloatingParams(){
+	public FloatingParams() {
 	}
 
 	public FloatingParams(int width, int height, int x, int y, int type) {
@@ -28,7 +41,7 @@ public class FloatingParams {
 		this.type = type;
 	}
 
-	public static FloatingParams getDefault(DisplayMetrics dm){
+	public static FloatingParams getDefault(DisplayMetrics dm) {
 		FloatingParams options = new FloatingParams();
 		options.width = (int) (dm.widthPixels * 2F / 3F);
 		options.height = (int) (options.width * 9F / 16F);
